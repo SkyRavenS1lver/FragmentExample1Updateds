3da54a8aa47b3467d4a4929cc2af6faefc2e83f5
SkyRavenS1lver <revandra.aryo.dwi.krisnandaru@mail.ugm.ac.id>
2023-02-22 14:08:45 +0700

    init commit

--- app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
+++ app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
@@ -2,11 +2,15 @@ package com.example.fragmentexample1updated;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,15 +18,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class SimpleFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public static final int YES = 0;
+    public static final int NO = 1;
 
     public SimpleFragment() {
         // Required empty public constructor
@@ -40,8 +37,7 @@ public class SimpleFragment extends Fragment {
     public static SimpleFragment newInstance(String param1, String param2) {
         SimpleFragment fragment = new SimpleFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,9 +46,30 @@ public class SimpleFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final RadioGroup radioGroup =  view.findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                View radioButton = radioGroup.findViewById(i);
+                int index = radioGroup.indexOfChild(radioButton);
+                TextView textView = view.findViewById(R.id.fragment_header);
+                switch (index){
+                    case YES :
+                        textView.setText(R.string.yes_message);break;
+                    case NO :
+                        textView.setText(R.string.no_message);break;
+                    default:
+                        break;
+                }
+            }
+        });
     }
 
     @Override
